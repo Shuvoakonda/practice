3e3aca94cb5d7e6e3d81156eeda770868dcbd901
Kazi Rayhan <thisiskazi@gmail.com>
2022-10-26 13:54:41 +0600

    first commit

---
+++ routes/web.php
@@ -0,0 +1,9 @@
+<?php
+
+use App\Http\Controllers\PagesController;
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/', [PagesController::class, 'home'])->name('home');
+Route::get('about', [PagesController::class, 'about'])->name('about');
+Route::get('contact', [PagesController::class, 'contact'])->name('contact');
